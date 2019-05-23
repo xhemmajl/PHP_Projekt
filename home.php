d3d65576b3b8f4cf3ffe76609a67f31828b25f06
Valton <valtoni.gl@live.com>
2019-05-23 05:51:01 -0700

    Update home.php

--- home.php
+++ home.php
@@ -10,3 +10,15 @@ include('header.php');
 <title>Lajme Sporti</title>
 <link rel="stylesheet" href="style.css">
 </head>
+
+<body>
+<div class="Divigjelbert"> 
+	<div class="anamajt"> 
+		<h2 style="color:white;">  Betisi fiton në Camp Nou spektaklin<br>e golave ndaj Barcelonës dhe Messit </h2>
+		<p>Barcelona është mposhtur në shtëpi nga Real Betisi<br>
+			Ata humben me rezultat 3-4 ne Camp Nou<br>
+			në ndeshjen e vlefshme për javën e 12-të të La Ligas.<br></p>
+		<div class="imazhii">
+			<img src="foto/barcelona2.jpg">
+		</div>
+		</div>
