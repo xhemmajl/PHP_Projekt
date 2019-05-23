e669ff3640f3d823e9debea5e61379ebdef629aa
Valton <valtoni.gl@live.com>
2019-05-23 05:50:07 -0700

    Update home.php

---
+++ home.php
@@ -0,0 +1,12 @@
+<?php 
+//include('server.php');
+
+include('header.php');
+?>
+
+<!DOCTYPE html>
+<html manifest="offline.chaching">
+<head>
+<title>Lajme Sporti</title>
+<link rel="stylesheet" href="style.css">
+</head>
