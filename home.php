5c9c4f820c52b8940c8da1463d660958adbbba02
xhemmajl <44348785+xhemmajl@users.noreply.github.com>
2019-05-25 01:40:53 +0200

    Update home.php

--- home.php
+++ home.php
@@ -15,9 +15,10 @@ include('header.php');
 <div class="Divigjelbert"> 
 	<div class="anamajt"> 
 		<h2 style="color:white;">  Betisi fiton në Camp Nou spektaklin<br>e golave ndaj Barcelonës dhe Messit </h2>
-		<p>Barcelona është mposhtur në shtëpi nga Real Betisi<br>
-			Ata humben me rezultat 3-4 ne Camp Nou<br>
-			në ndeshjen e vlefshme për javën e 12-të të La Ligas.<br></p>
+		<?php
+		$arr=array ('Barcelona','është','mposhtur','në','shtëpi','nga','Real','Betisi','<br>','Ata','humben','me','rezultat','3-4','ne','Camp Nou','<br>','në','ndeshjen','e','vlefshme','për','javën','e','12-të','të','La Ligas');
+		echo implode(" ",$arr);
+		?>
 		<div class="imazhii">
 			<img src="foto/barcelona2.jpg">
 		</div>
