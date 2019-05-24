6f932f87490ceb340fd985edeff5959ef97500fb
RudinaGjikokaj <43991137+RudinaGjikokaj@users.noreply.github.com>
2019-05-25 01:01:18 +0200

    Create Football.php

---
+++ Football.php
@@ -0,0 +1,515 @@
+<?php 
+//include('server.php');
+
+include('header.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title> Lajme Sporti - Futboll </title>
+<link rel="stylesheet" href="fbstyle.css">
+<style>
+#more {display: none;
+}
+#more1{display:none;}
+#more2{display:none;}
+#more3{display:none;}
+#more4{display:none;}
+#more5{display:none;}
+#more6{display:none;}
+#more7{display:none;}
+#more8{display:none;}
+
+
+#myBtn{
+	float:left;
+}
+#myBtn1{
+	float:left;
+}
+#myBtn2{
+	float:left;
+}
+#myBtn3{
+	float:left;
+}
+#myBtn4{
+	float:left;
+}
+#myBtn5{
+	float:left;
+}
+#myBtn6{
+	float:left;
+}
+#myBtn7{
+	float:left;
+}
+#myBtn8{
+	float:left;
+}
+</style>
+<script>
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Lexo më shumë"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Lexo më pak"; 
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunction1() {
+  var dots = document.getElementById("dots1");
+  var moreText = document.getElementById("more1");
+  var btnText = document.getElementById("myBtn1");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Lexo më shumë"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Lexo më pak"; 
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunction2() {
+  var dots = document.getElementById("dots2");
+  var moreText = document.getElementById("more2");
+  var btnText = document.getElementById("myBtn2");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Lexo më shumë"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Lexo më pak"; 
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunction3() {
+  var dots = document.getElementById("dots3");
+  var moreText = document.getElementById("more3");
+  var btnText = document.getElementById("myBtn3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Lexo më shumë"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Lexo më pak"; 
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunction4() {
+  var dots = document.getElementById("dots4");
+  var moreText = document.getElementById("more4");
+  var btnText = document.getElementById("myBtn4");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Lexo më shumë"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Lexo më pak"; 
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunction5() {
+  var dots = document.getElementById("dots5");
+  var moreText = document.getElementById("more5");
+  var btnText = document.getElementById("myBtn5");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Lexo më shumë"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Lexo më pak"; 
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunction6() {
+  var dots = document.getElementById("dots6");
+  var moreText = document.getElementById("more6");
+  var btnText = document.getElementById("myBtn6");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Lexo më shumë"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Lexo më pak"; 
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunction7() {
+  var dots = document.getElementById("dots7");
+  var moreText = document.getElementById("more7");
+  var btnText = document.getElementById("myBtn7");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Lexo më shumë"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Lexo më pak"; 
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunction8() {
+  var dots = document.getElementById("dots8");
+  var moreText = document.getElementById("more8");
+  var btnText = document.getElementById("myBtn8");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Lexo më shumë"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Lexo më pak"; 
+    moreText.style.display = "inline";
+  }
+}
+</script>
+</head>
+<body>
+	<section id="timeline">
+	<div class="t1-item">
+		<div class="t1-bg" style="background-image:url(foto/lukaku.jpg)"></div>
+		<div class="t1-year">
+			<p class="f2 heading">Manchester United</p>
+			</div>
+			<div class="t1-content">
+			<h1>Unitedi fiton lehtësisht ndaj Fulhamit, gati për derbin ndaj Liverpoolit </h1>
+			<p> 
+				Manchester Unitedi i është kthyer fitoreve në Ligën Premier,
+				 duke fituar kështu ndaj Fulhamit me rezultat 4-1.
+}
+
+Djajtë e Kuq ishin dukshëm më të mirë 
+dhe kjo ishte një fitore e lehtë.
+Goli i parë u shënua nga Ashley Young në minutën e 13-të,
+ ndërsa në minutën e 28-të e shënoi edhe Juan Mata.
+ Nga fundi i pjesës së parë shënoi edhe Romelu Lukaku (42′)
+  dhe me rezultatin 3-0 përfunduan 45 minutat e parë.
+
+Ishte Aboubakar Kamara që shënoi golin e vetëm
+ në takim për mysafirët në minutën e 67-të.
+Për ta bërë këtë fitore edhe më të bukur,
+ në minutën e 82-të shënoi edhe Marcus Rashford.
+
+
+Pas kësaj fitore Djajtë e Kuq renditen në vendin e gjashtë me 26 pikë,
+ kurse Fulhami i fundit në tabelë me vetëm nëntë pikë.</p>
+ </div>
+	</div>
+	<div class="t1-item">
+		<div class="t1-bg" style="background-image:url(foto/messi.jpg)"></div>
+		<div class="t1-year">
+			<p class="f3 heading">Barcelona</p>
+		</div>
+		<div class="t1-content">
+			<h1>
+				Barcelona shkatërron Espanyolin në derbin katalunas, Messi shkëlqen me dy gola nga gjuajtja e lirë </h1>
+			<p> 
+				Barcelona ka fituar në udhëtim derbin katalunas
+				 ndaj Espanyolit me rezultat të thellë 4-0.
+				Goli i parë i ndeshjes erdhi në minutën e 17-të të ndeshjes me Lionel Messin, 
+				i cili shënoi për 1-0 një gol të bukur nga gjuajtja e lirë nga rreth 25 metra.
+
+Për epërsinë 2-0 u përkujdes Ousmane Dembele (26′) pas asistimit të Messit,
+ ndërsa Luis Suarez (45′) shënoi për thellimin e epërsinë në 3-0.
+Në këtë pjesë ishte përsëri ylli argjentinas Messi, i cili shkëlqeu duke shënuar golin e dytë nga gjuajtja e lirë 
+pasi në minutën e 65-të shënoi për 0-4 dhe vulosi rezultatin përfundimtar.</p>
+		</div>
+</div>
+<div class="t1-item">
+		<div class="t1-bg" style="background-image:url(foto/hazard.jpg)"></div>
+		<div class="t1-year">
+			<p class="f2 heading">Chelsea</p>
+</div>
+		<div class="t1-content">
+			<h1> Hazard ende beson se Chelsea është në garë për titull </h1>
+			<p> 
+				Eden Hazard këmbëngul se ende janë në garën
+				 për titullin e Premier Ligës.
+Chelsea ka triumfuar gjatë ditës së djeshme
+ kundër Brightonit me rezultat 2-1 në kuadër të Ligës Premier.
+ Kjo ka bërë që belgu të mendoj se skuadra e tij 
+ nuk i ka humbur shpresat për titullin e më të mirit në Angli.
+ “Më pëlqen të shënoj, por nuk është e rëndësishme 
+ për mua, gjëja më e rëndësishme është të fitosh lojërat”.
+
+“Kjo është arsyeja pse jam në fushë, ne e dinim para ndeshjes
+ që duhej të shënonim një gol të tretë”.
+
+“Ne e menaxhuam lojën mirë. Ne duam të jemi në krye të renditjes në fund të sezonit,
+ ne e dimë se Man City dhe Liverpool janë skuadra të mira,
+  por nuk duam që boshllëku të jetë i madh”.
+ </p>
+		</div>
+	</div>
+	<div class="t1-item">
+		<div class="t1-bg" style="background-image:url(foto/romaa.jpg)"></div>
+		<div class="t1-year">
+			<p class="f2 heading">Roma</p>
+		</div>
+		<div class="t1-content">
+			<h1> Roma dhe Interi ndajnë pikët në kryendeshjen e javës </h1>
+			<p> 
+				Ndeshja në mes Romës dhe Interit u mbyll me rezultat 2-2.
+				Rezultati u zhbllokua në minutën e 37-të, kur pas një harkimi të Danilo D’Ambrosios në zonë,
+				 Keita Balde goditi me të parën për ta futur topin në rrjetë.
+
+Në vazhdim, Samir Handanovic priti për mrekulli një gjuajtje të lirë të Aleksandar Kolarov
+ dhe mysafirët shkuan në pushim me epërsi.
+Mirëpo, vetëm disa minuta brenda pjesës së dytë (51′), 
+Cengiz Under realizoi një gol shumë të bukur për t’i barazuar shifrat.
+Pas një harkimi nga këndi të Marcelo Brozovic,
+ kapiteni Mauro Icardi kërceu më lartë për të shënuar me kokë (66′) dhe për t’ia kthyer Interit avantazhin.
+ Porse, Brozovic e ndali topin me dorë në zonë dhe penalltinë
+  e akorduar e konvertoi në gol Aleksandar Kolarov (74′).</p>
+		</div>
+	</div>
+
+
+    
+
+</section>
+<div class="RreshtiLajmev">
+	<div class="Lajmi1">
+		<h3>Messi merr Këpucën e Artë.</h3>
+		<p>Sot (e martë) ishte dita kur Lionel Messi do të pranonte edhe zyrtarisht Këpucën e Artë,
+		 si golashënuesi më i mirë në Evropës për sezonin 2017/18.<span id="dots">...</span><span id="more">
+
+Argjentinasi shënoi 34 gola në sezonin e fundit në La Liga,
+ ndërsa kjo ishte hera e pestë që ai e fiton këtë çmim prestigjioz.
+ E bukura është se 31-vjeçari me këtë rast bëhet lojtari i parë në histori që e fiton pesë herë Këpucën e Artë.
+ Ceremonia e ndarjes së çmimit u mbajt në qytetin e Barcelonës, 
+ ndërsa Leo nuk harroi të falënderojë edhe bashkëlojtarët për arritjet e tij.
+
+“Nuk i kam pritur gjithë këtu kur fillova, ëndrra ime ishte të isha një profesionit
+ dhe të triumfoja në futboll. Jam në ekipin më të mirë në botë dhe kam bashkëlojtarët më të mirë në botë
+  dhe kjo i bën gjërat më të lehta që unë t’i fitojë këto çmime”, ka thënë Messi pasi pranoi çmimin.
+  
+		</span></p>
+		<button onclick="myFunction()" id="myBtn">Lexo më shumë</button>
+		
+		<div class="imazhi">
+			<img src="foto/lajmi1.jpg">
+		</div>		
+	</div>
+	<div class="Lajmi2">
+		<h3>Juventusi takohet me Raiolan</h3>
+		<p>Juventusi është takuar me agjentin Mino Raiola sot (e diel),
+		 për të diskutuar një rinovim për talentin Moise Kean, por edhe Paul Pogba dhe Matthijs de Ligt.<span id="dots1">...</span><span id="more1">
+
+Agjenti i njohur ka një marrëdhënie të mirë me Bianconerët,
+ pasi ai përfaqësoi nënkryetarin Pavel Nedved gjatë ditëve të tij si lojtar.t.
+ Sipas mediumit italian Sky Italia, drejtorët e Juventusit u takuan sot me Raiolan, 
+ me temën kryesore të diskutimit për një rinovim për sulmuesin e ri Kean.
+
+Besohet se ka pasur hapa përpara për t’u lidhur me italianin dhe ka pasur edhe
+ diskutime për të ardhmen e lojtarëve të tjerë të akademisë.
+ Palët diskutuan gjithashtu edhe për mbrojtësin e talentuar holandez De Ligt, 
+ i cili mori sot çmimin ‘Djaloshi i Artë’, por sipas Tuttosport, Barcelona duket se është në avantazh për ta nënshkruar atë.
+ E ardhmja e francezit është e lidhur me atë të menaxherit të Unitedit Jose Mourinho,
+  pasi nëse portugezi lë Old Traffordin atëherë Pogba ka të ngjarë të qëndrojë.
+		</span></p>
+			<button onclick="myFunction1()" id="myBtn1">Lexo më shumë</button>
+		<div class="imazhi">
+			<img src="foto/lajmi2.jpg">
+		</div>
+	</div>
+	<div class="Lajmi3">
+		<h3>Interi mposht Udinesen fal golit Icardit (Rastet dhe Golat)</h3>
+		<p>Pas barazimit me Romën dhe humbjes nga Juventusi, 
+			Interi i është kthyer fitores në Serie A pasi arriti të triumfojë ndaj Udineses me rezultat 1-0.<span id="dots2">...</span><span id="more2">
+				Zikaltrit ishin më të mirë dhe fitorja e shumë pritur nga skuadra e Luciano Spallettit ishte e merituar.
+				Pjesa e parë kryesisht u zhvillua në mesfushë
+				 dhe jo me shumë raste serioze për gol.
+
+Rastin e parë serioz për gol e kishte Mauro Icardi (66′),
+ por argjentinasi gjuajti jashtë portës me kokë pas harkimit të bukur të Keita Baldes.
+ Ishte kapiteni zikaltër që shënoi në minutën e 76-të nga pika e bardhë në stilin Panenka,
+  duke befasuar kështu portierin kundërshtar.
+
+Në pjesën e mbetur skuadra nga Giuseppe Meazza
+ u vendos mirë dhe nuk lejoi të befasohet nga mysafirët.
+ Rastet dhe golin e shenuar mund ti ndiqni në videon më poshtë.
+ <video width="500" height="300" controls>
+  <source src="foto/rastetinter.mp4" type="video/mp4">
+  </video>
+
+		</span></p>
+		<button onclick="myFunction2()" id="myBtn2">Lexo më shumë</button>
+
+		<div class="imazhi">
+			<img src="foto/lajmi4.jpg">
+		</div>
+	</div>
+
+</div>
+<div class="RreshtiLajmev">
+	<div class="Lajmi1">
+		<h3>Marrëveshja për menaxhimin e stadiumit ‘Fadil Vokrri’</h3>
+		<p>Kryetari i Federatës së Futbollit të Kosovës, 
+			Agim Ademi ka pritur sot në zyrën e tij kryetarin e Komunës së Prishtinës, Shpend Ahmetin.<span id="dots3">...</span><span id="more3">
+				Në këtë takim ishin të pranishëm edhe sekretari i përgjithshëm, Eroll Salihu dhe zëvendëssekretari, 
+				Arianit Ahmeti si dhe këshilltari i kryetarit Ahmeti, Adonis Tahiri. Gjatë këtij takimi u diskutuan disa çështje të rëndësishme.
+				Fillimisht u bisedua rreth menaxhimit të stadiumit “Fadil Vokrri” 
+				ku edhe u arrit marrëveshja që me këtë stadium veçmas gjatë ciklit kualifikues për Euro 2020 të menaxhojë FFK-ja në bashkëpunim me komunën.
+
+Ndërkohë, këtë marrëveshje e ka miratuar edhe ministri i sportit, Kujtim Gashi,
+ i cili është takuar më pas me presidentin Ademi dhe kryetarin Ahmeti.
+
+
+		</span></p>
+		<button onclick="myFunction3()" id="myBtn3">Lexo më shumë</button>
+		<div class="imazhi">
+			<img src="foto/lajmi3.jpg">
+		</div>		
+	</div>
+	<div class="Lajmi2">
+		<h3>Milani dhe Interi në garë për Filipe Luis</h3>
+		<p>Mbrojtësi i Atletico Madridi, Filipe Luis
+		 ka përfunduar në radarin e dy skuadrave të mëdha italiane, Milanit dhe Interit.<span id="dots4">...</span><span id="more4">
+		 	Braziliani 33-vjeçar ka kontratë të vlefshme deri në verën e vitit 2019 dhe ajo kontratë përfundon në fund të sezonit.
+
+Kjo situatë mund të shtyjë mbrojtësin brazilian
+ të largohet nga Wanda Metropolitano dhe të tentoj të gjej një ekip të ri.
+
+Sipas mediumit italian Calciomercato, 
+Filipe Luis pëlqehet shumë nga dy skuadrat italiane, Milani dhe Interi.
+Klubet nga qyteti i modës janë duke e monitoruar situatën e mbrojtësit të majtë
+ dhe pritet të tentojnë transferimin e tij.
+
+Të dy skuadrat po kanë probleme me krahët e mbrojtjes dhe Luis do të shtone cilësinë dhe përvojën në atë krahë.
+		</span></p>
+		<button onclick="myFunction4()" id="myBtn4">Lexo më shumë</button>
+		<div class="imazhi">
+			<img src="foto/lajmi5.jpg">
+		</div>
+	</div>
+	<div class="Lajmi3">
+		<h3 class="gjel">Mourinho synon tri transferime në janar</h3>
+		<p>Jose Mourinho ka etiketuar tri përforcime për afatin kalimtar të janarit në ekipin e Manchester Unitedit.
+			Manchester United nuk e ka nisur <span id="dots5">...</span><span id="more5">aspak mirë këtë sezon, pasi është ende larg objektiveve në kampionat,
+			 por nuk po bind as në Ligën e Kampionëve.
+			 Sigurisht që reparti që ka nevojë më shumë për përforcime është mbrojtja,
+			  por portugezi synon përforcime edhe në mesfushë.
+			  <img src="foto/godin.jpg" style="height:300px;width:500px;"> <br>
+			  Sipas mediumit anglez Daily Star, Mourinho po bën presion për transferimin e mbrojtësit
+			   të Atletico Madridit, Diego Godin dhe atij të Chelseat, Gary Cahill.
+			    <img src="foto/cahill.jpg" style="height:300px;width:500px;"> <br>
+			    Në anën tjetër, në mesfushë pëlqehet shumë italiani i Romës, Lorenzo Pellegrini.
+
+Transferimi i mbrojtësve Godin dhe Cahill është më i lehtë
+ shkaku i moshës dhe kontratës afër përfundimin, ndërsa për Pellegrinin do të jetë më vështirë.
+
+		</span></p>
+		<button onclick="myFunction5()" id="myBtn5">Lexo më shumë</button>
+		<div class="imazhi">
+			<img src="foto/lajmi6.jpg">
+		</div>
+	</div>
+
+</div>
+<div class="RreshtiLajmev">
+	<div class="Lajmi1">
+		<h3>Real 0-3 CSKA: Notat e lojtarëve, rastet dhe golat.</h3>
+		<p>Real Madridi humbi ndeshjen e fundit në fazën e grupeve të Ligës së Kampionëve
+		 ndaj CSKA Moskës me rezultat 0-3.<span id="dots6">...</span><span id="more6">
+		 	Golat për mysafirët i shënuan Fedor Chalov (37), Georgi Schennikov (43′) dhe Amor Sigurdsson (73′).
+		 	Më i miri për CSKA Moskën ishte Nikola Vlasic, i cili përveç paraqitjes së mirë arriti të dhurojë edhe një asistim.
+
+Te Reali më i miri ishte Marco Asensio, por edhe Vinicius Junior e Federico Valverde.
+
+Ata që dëshpëruan më shumë ishin mbrojtësit, 
+në përjashtim të Jesus Vallejos, ndërsa më poshtë keni notat e lojtarëve:
+Real Madrid : <br>
+ <img src="foto/realnota.jpg" style="height:300px;width:500px;"> <br>
+ CSKA Moska: <br>
+ <img src="foto/notacska.jpg" style="height:300px;width:500px;"> <br>
+ Rastet dhe golat e ndeshjes i gjeni ne videon më poshtë:
+  <video width="500" height="300" controls>
+  <source src="foto/rmacska.mp4" type="video/mp4">
+  </video>
+</span></p>
+<button onclick="myFunction6()" id="myBtn6">Lexo më shumë</button>
+		<div class="imazhi">
+			<img src="foto/lajmi8.jpg">
+		</div>		
+	</div>
+	<div class="Lajmi2">
+		<h3>Arsenali favorit për transferimin e Pepes</h3>
+		<p>Klubi anglez, Arsenali është favorit për të nënshkruar me anësorin 
+			sulmues të skaudrës franceze, Lille, Nicolas Pepe.<span id="dots7">...</span><span id="more7">
+				Topçinjtë pritet të angazhohen në tregun e transferimit
+				 të janarit për ndonjë përforcim eventual në sulm.
+				 Kjo pasi, sulmuesi anglez Danny Welbeck është jashtë fushave për disa muaj shkaku i një lëndimi në kyçin e këmbës.
+				 Barcelona gjithashtu ka qenë e interesuar për 23-vjeçarin,
+				  së bashku me gjigantin anglez Manchester Cityn.
+
+Megjithatë, L’Equipe raporton se Arsenali është favorit
+ për të nënshkruar me talentin e madh nga Bregu i Fildishtë.
+		</span></p>
+		<button onclick="myFunction7()" id="myBtn7">Lexo më shumë</button>
+
+		<div class="imazhi">
+			<img src="foto/lajmi7.jpg">
+		</div>
+	</div>
+	<div class="Lajmi3">
+		<h3>Ramos, mbrojtësi qendror më i miri në botë</h3>
+		<p>Për tetë sezone të njëpasnjëshme, kapiten i Real Madridit dhe Spanjës,
+		 Sergio Ramos pritet të jetë në formacionin më të mirë <span id="dots8">...</span><span id="more8"> të FIFA-s.
+		 	Të gjithë fituesit e çmimeve më elitare të futbollit do të shpallen në ceremoninë ‘The Best FIFA’,
+		 	 e cila do të mbahet sonte në Londër.
+		 	 Ramos është përzgjedhur në ekipin më të mirë në botë nëntë herë dhe ka qenë gjithmonë i pranishëm që nga viti 2011, 
+		 	 ndërsa Marca e konsideron si më të mirin.
+
+Në vitin 2008 ai u zgjodh së bashku me Gerard Piquen, por mungoi dy herë radhazi.<br>
+<img src="ramos.jpg" style="height:300px;width:500px;"> <br>
+Ramos është më shumë se sa i denjë për një vend në formacion, pasi është është i njohur për stilin e tij të rrezikshëm të lojës,
+ kuptimin e lojës, shpirtin e tij konkurrues dhe angazhimin në çdo kohë.
+
+Kjo është mënyra se si ai luan dhe nuk do të ndryshojë kurrë.
+
+Mbrojtësi tani ka 19 tituj në emrin e tij, duke përfshirë dy
+ Kampionate Evropiane dhe një Kupa Bote me Spanjën .
+		</span></p>
+		<button onclick="myFunction8()" id="myBtn8">Lexo më shumë</button>
+		<div class="imazhi">
+			<img src="foto/lajmi9.jpg">
+		</div>
+	</div>
+
+</div>
+<?php
+include('footer.php');
+?>
