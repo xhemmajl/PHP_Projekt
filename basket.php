c68bc57ffb14d90ed46a613d7791ac5fa822d959
Valton <valtoni.gl@live.com>
2019-05-25 08:23:13 -0700

    Update basket.php

--- basket.php
+++ basket.php
@@ -104,7 +104,41 @@ include('header.php');
 				</p>
 
 
-				
+				<button type="button" id="read" onclick="read()">Lexo me shume</button>
+				<script type="text/javascript">
+					var i=0;
+					function read(){
+						if(!i){
+							document.getElementById("more").style.display="inline";
+							document.getElementById("dots").style.display="none";
+							document.getElementById("read").innerHTML="Lexo me pake";
+							i=1;
+						}
+						else{
+							document.getElementById("more").style.display="none";
+							document.getElementById("dots").style.display="inline";
+							document.getElementById("read").innerHTML="Lexo me shume";
+							i=0;
+
+						}
+
+
+						}
+					
+
+
+				</script>
+	 			<img src="foto/basket1.jpg">
+
+
+
+
+
+			</div>
+
+
+
+			
 
 
 
