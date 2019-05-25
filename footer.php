a7e0810d5e6ba59df55b3139ab7965ef5d50f47c
Taulant8 <44265863+Taulant8@users.noreply.github.com>
2019-05-25 18:10:25 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	
+</head>
+
+
+<body>
+<!-- page footer -->  
+<div class="footer" style="text-align:center">
+<a href="web.php" ><p class="foot" "> Find us on Twitter </a> 
+<p class="foot">&copy;Sport Page</p>
+<p class="foot">Prishtine 2019</p>
+
+
+
+<br><br>
+</div>
+
+</body>
+</html>
