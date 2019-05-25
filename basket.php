293a453d86545fc5e5284d1238f225cc3d1deece
Valton <valtoni.gl@live.com>
2019-05-25 08:19:28 -0700

    Update basket.php

--- basket.php
+++ basket.php
@@ -18,6 +18,21 @@ include('header.php');
 
 <body>
 
+<section id="timeline">
+		<div class="t1-item">
+		<div class="t1-bg" style="background-image:url(foto/lakerslogo.jpg)"></div>
+		<div class="t1-year">
+			<p class="f2 heading">LAKERS</p>
+		</div>
+		<div class="t1-content">
+			<h1>Los Angeles Lakers triumfon kundër Miami Heat </h1>
+			<p>Mbrëmë janë zhvilluar 11 ndeshje në ligën më të fortë të basketbollit në botë.
+			Los Angeles Lakers ka shënuar fitoren e radhës në këtë sezon në NBA pas një kampionati të dështuar vitin e kaluar.
+			 </p>
+		</div>
+	        </div>
+
+	        
 
 
 
