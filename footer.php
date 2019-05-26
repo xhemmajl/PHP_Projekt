8eea68c93fb1af3d250d2b3dee76d1b88044b828
xhemmajl <44348785+xhemmajl@users.noreply.github.com>
2019-05-26 23:42:52 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,13 +2,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-	
+	<style>
+		body{
+background-color:white;}	
+.footer{
+	background-color:black;
+	padding: 10px;
+
+}
+.foot{
+	color:white; 
+    font-size:14px; 
+	font-weight:Bold;
+    text-align:center;                      
+    font-family:Courier New; 
+    font-weight:italic;
+}
+</style>
 </head>
 
 
 <body>
 <!-- page footer -->  
-<div class="footer" style="text-align:center">
+<div class="footer">
 <p class="foot">&copy;Sport Page</p>
 <p class="foot">Prishtine 2019</p>
 
